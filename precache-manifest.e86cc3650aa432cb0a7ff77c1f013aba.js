5b6730e6c1083f9efbe74b481206c2c73f11baeb
Jack Smith <jack.smith@elucidata.co.uk>
2020-06-15 22:40:35 +0100

    Updates

---
+++ precache-manifest.e86cc3650aa432cb0a7ff77c1f013aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d61fb28068a7f79d2d7341223950a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf03919bc74c273b960b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c5f6ef85e739bdf20803",
+    "url": "/static/css/main.36b0b72b.chunk.css"
+  },
+  {
+    "revision": "cf03919bc74c273b960b",
+    "url": "/static/js/2.22faeda4.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.22faeda4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f6ef85e739bdf20803",
+    "url": "/static/js/main.d1c8a5bb.chunk.js"
+  },
+  {
+    "revision": "67a9db3451f3d2c5a7d6",
+    "url": "/static/js/runtime-main.1fb6946a.js"
+  }
+]);
